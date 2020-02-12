6110f41ccc66c7c84a11f188a06f5e9b18be59ed
Lauren Rose <larose@radford.edu>
2020-02-11 23:44:22 -0500

    create list of members to add to channel from user input

--- src/main/java/BotEventListener.java
+++ src/main/java/BotEventListener.java
@@ -1,13 +1,12 @@
-import net.dv8tion.jda.api.entities.Category;
-import net.dv8tion.jda.api.entities.Guild;
-import net.dv8tion.jda.api.entities.GuildChannel;
-import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.internal.entities.GuildImpl;
 import net.dv8tion.jda.internal.entities.TextChannelImpl;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -46,13 +45,45 @@ public class BotEventListener extends ListenerAdapter {
     private void startAParty(Guild guild, MessageReceivedEvent event, String[] partyDetails) {
         if (partyDetails.length >= 2) {
             managePrivateCategory(guild);
-            event.getChannel().sendMessage("Let's have a party!").queue();
-            guild.createTextChannel(partyDetails[1]).complete();
-            TextChannel tc = guild.getTextChannelsByName(partyDetails[1], false).get(0);
-            tc.sendMessage("Welcome!").queue();
+            String channelName = partyDetails[1];
+            try {
+                ArrayList<Member> privateChannelMembersList = getPrivateChannelUsers(guild, event.getAuthor(), partyDetails);
+                addMembersToChannel(privateChannelMembersList);
+                event.getChannel().sendMessage("Let's have a party!").queue();
+                guild.createTextChannel(channelName).complete();
+                TextChannel tc = guild.getTextChannelsByName(channelName, false).get(0);
+                tc.sendMessage("Welcome!").queue();
+            } catch (MemberNotFoundException e) {
+                event.getChannel().sendMessage("Hmm, " + e.getNotFoundName() + " can't be found!").queue();
+            }
         } else {
             event.getChannel().sendMessage("You didn't tell me where the party's at!").queue();
         }
+    }
+
+    private ArrayList<Member> parseUserNames(Guild guild, String[] names) throws MemberNotFoundException {
+        ArrayList<Member> users = new ArrayList<>();
+        for (String userName : names) {
+            List<Member> currentMembers = guild.getMembersByName(userName, true);
+            if (!currentMembers.isEmpty()) {
+                Member tempUser = currentMembers.get(0);
+                users.add(tempUser);
+            } else {
+                throw new MemberNotFoundException(userName);
+            }
+        }
+        return users;
+    }
+
+    private ArrayList<Member> getPrivateChannelUsers(Guild guild, User requester, String[] allCommands) throws MemberNotFoundException {
+        String[] nameCommands = new String[allCommands.length - 1];
+        allCommands[1] = requester.getName();
+        System.arraycopy(allCommands, 1, nameCommands, 0, allCommands.length-1);
+        return parseUserNames(guild, nameCommands);
+    }
+
+    private void addMembersToChannel(ArrayList<Member> members) {
+        // do stuff
     }
 
     private void managePrivateCategory(Guild guild) {
